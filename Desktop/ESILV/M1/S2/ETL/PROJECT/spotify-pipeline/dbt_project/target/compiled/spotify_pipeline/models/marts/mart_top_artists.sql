4fdd20bc1bfd6bbde4732b29aec734f71110328a
Chrisa Mendoza <mendozachrisa21@gmail.com>
2026-06-19 08:31:54 +0200

    Revert "docs: add architecture diagram to README"
    This reverts commit 8890bb915fbe39f94dc13557a44abd635b05565a.

--- Desktop/ESILV/M1/S2/ETL/PROJECT/spotify-pipeline/dbt_project/target/compiled/spotify_pipeline/models/marts/mart_top_artists.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    fetch_date,
-    country,
-    artist_name,
-    COUNT(DISTINCT track_id)   AS nb_tracks_in_top50,
-    ROUND(AVG(popularity), 1)  AS avg_popularity
-FROM "spotify_db"."analytics"."stg_tracks"
-GROUP BY fetch_date, country, artist_name
-ORDER BY fetch_date DESC, avg_popularity DESC
